415e83492cc0fe2fbe723fde3b410542fc5f79e8
Melchioris <MELCHIJ@ccf.org>
2024-06-06 14:25:58 -0400

    Added parameter for subitems in breadcrumb

--- src/MudBlazor/Components/Breadcrumbs/BreadcrumbItem.cs
+++ src/MudBlazor/Components/Breadcrumbs/BreadcrumbItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace MudBlazor
 {
@@ -31,18 +32,25 @@ namespace MudBlazor
         public string? Icon { get; }
 
         /// <summary>
+        /// List of subitems for current BreadCrumbItem
+        /// </summary>
+        public List<BreadcrumbItem>? SubItems { get; }
+
+        /// <summary>
         /// Creates a new instance.
         /// </summary>
         /// <param name="text">The text to display for this item.</param>
         /// <param name="href">The URL to navigate to when this item is clicked.</param>
         /// <param name="disabled">Whether the item cannot be clicked.</param>
         /// <param name="icon">The custom icon to display for this item.</param>
-        public BreadcrumbItem(string text, string? href, bool disabled = false, string? icon = null)
+        /// /// <param name="subItems">The subitems for the current BreadCrumbItem.</param>
+        public BreadcrumbItem(string text, string? href, bool disabled = false, string? icon = null, List<BreadcrumbItem>? subItems = null)
         {
             Text = text;
             Disabled = disabled;
             Href = href;
             Icon = icon;
+            SubItems = subItems;
         }
     }
 }
